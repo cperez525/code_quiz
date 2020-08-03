b222144842e5ccb2add683a1f5100c0d8aa351c4
Cris Perez <cristino.perez525@gmail.com>
2020-08-03 17:50:04 -0500

    Introduced several different test questions, set up question and answers rendering, introduced logic to cycle through questions and answers

--- script.js
+++ script.js
@@ -1,43 +1,147 @@
-var question1 = {
-    question: "How are you today?",
-    Incorrect1: "Meh",
-    Incorrect2: "Terrible",
-    Correct: "Alright",
-    Incorrect3: "Great"
-}
 
-var question1Values = Object.values(question1)
-console.log(question1Values)
+var testQuestions = [
+    {
+        question: "What symbols indicate a function/method?",
+        answers: [
+            "()",
+            "[]",
+            "/* */",
+            "''"
+        ],
+        correctAnswer: "()"
+    },
 
-var body = document.querySelector("body")
+    {
+        question: "Which is NOT type of variable?",
+        answers: [
+            "string",
+            "number",
+            "boolean",
+            "commit"
+        ],
+        correctAnswer: "function"
+    },
 
-function writeQuestions() {
+    {
+        question: "What, by default, separates indexes within an array?",
+        answers: [
+            "Spaces",
+            "Nothing",
+            "''",
+            ","
+        ],
+        correctAnswer: ","
+    },
 
-    var divEl = document.createElement("div")
-    body.appendChild(divEl)
+    {
+        question: "What is the extension of Javascript files?",
+        answers: [
+            ".javascript",
+            ".java",
+            ".js",
+            ".script"
+        ],
+        correctAnswer: ".js"
+    },
 
-    var ulEl = document.createElement("ul")
-    divEl.appendChild(ulEl)
+    {
+        question: "When was Javascript created?",
+        answers: [
+            "2012",
+            "1992",
+            "1995",
+            "2000"
+        ],
+        correctAnswer: "1995"
+    },
 
-    var question = document.createElement("li")
-    question.textContent = question1Values[0]
-    ulEl.appendChild(question)
+    {
+        question: "What was Javascript originally called?",
+        answers: [
+            "Windows.script",
+            "Mocha",
+            "funcscript",
+            "Java"
+        ],
+        correctAnswer: "Mocha"
+    },
 
-    for (var i = 1; i < question1Values.length; i++) {
-
-        var potAnswers = document.createElement("button")
-        potAnswers.setAttribute("id","button")
-        potAnswers.textContent = question1Values[i]
-        question.appendChild(potAnswers)
+    {
+        question: "Which of the following is true?",
+        answers: [
+            "Javascript is a strictly typed language",
+            "Javascript is multi-threaded",
+            "In Javascript, 'Null' is an object",
+            "A function cannot be assigned to a variable"
+        ],
+        correctAnswer: "In Javascript, 'Null' is an object"
     }
-    
+
+]
+
+// Question counter
+var q = 0
+
+// Quiz text variables to display on page for quiz
+var currentQuestion = testQuestions[q].question
+var currentRightAns = testQuestions[q].correctAnswer
+
+// Creating page elements and assigning appropriate quiz text
+var body = $("body")
+
+var mainEl = $("<main>")
+body.append(mainEl)
+
+var questionEl = $("<h1>")
+mainEl.append(questionEl)
+
+var answerList = $("<ul>")
+mainEl.append(answerList)
+
+function renderQuestion() {
+
+    var currentQuestion = testQuestions[q].question
+    questionEl.text(currentQuestion)
 }
 
-writeQuestions()
+function renderAnswers() {
+    for (var a = 0; a < 4; a++) {
 
 
+        var answerListItem = $("<li>")
+        answerList.append(answerListItem)
 
+        var ansBtn = $("<button>")
+        ansBtn.attr("id", "button")
 
+        var answerChoices = testQuestions[q].answers
+        ansBtn.text(answerChoices[a])
+        answerListItem.append(ansBtn)
+
+        console.log($("button").text())
+
+        handleClick()
+    }
+}
+
+function handleClick() {
+    $("button").on("click", function () {
+
+        console.log($(this).text())
+        q++
+
+        if (q < testQuestions.length) {
+
+            $("li").remove()
+
+            renderQuestion()
+            renderAnswers()
+            console.log($("button").text())
+        }
+    })
+}
+renderQuestion()
+renderAnswers()
 
 
 
